b4370657269ccd05918dafeb763788f83e1acef3
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-03-26 21:49:14 +0100

    Feat: addEventListener click resetta

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const generaBtn = document.getElementById("generaBtn")
 
 // aggiungi un evento al bottone 
 
-generaBtn.addEventListener("click", function() {
-    
-    
+generaBtn.addEventListener("click", function () {
+
+
     // aggiungi nome utente
     const nomeUtente = document.getElementById("nomeUtente"); // object | null
     const nomeValue = nomeUtente.value; // stringa 
@@ -23,36 +23,52 @@ generaBtn.addEventListener("click", function() {
     const etaUtente = document.getElementById("etaUtente");
     const etaUteneteValue = etaUtente.value;
     console.log(etaUteneteValue);
-  
+
     // ESECUZIONE LOGICA
 
     // Stabilire il prezzo del biglietto in base ai Km percorsi
     let costoBiglietto = prezzoAlKmValue * 0.21; // number
     console.log("Prezzo intero", costoBiglietto);
-    document.getElementById("intero").innerHTML = costoBiglietto;
+    // document.getElementById("intero").innerHTML = costoBiglietto;
 
     // Creare una variabile sconto
 
     let sconto = 0; // number
 
-    // Se l'utente ha meno di 18 anni avrà uno sconto del 20%
-    // altrimenti se ha più di 65 anni avrà uno sconto del 40%
+    
 
-    if (etaUteneteValue === "minorenne") {
-        sconto = 20;
-    } else if (etaUteneteValue === "Over 65") {
-        sconto = 40;
-    }
+        // Se l'utente ha meno di 18 anni avrà uno sconto del 20%
+        // altrimenti se ha più di 65 anni avrà uno sconto del 40%
 
-    console.log ("sconto in %", sconto);
+        if (etaUteneteValue === "minorenne") {
+            sconto = 20;
+        } else if (etaUteneteValue === "Over 65") {
+            sconto = 40;
+        }
 
-    // Applicare lo sconto in base alla fascia d'eta
-    // creo variabile prezzoscontato
+        console.log("sconto in %", sconto);
 
-    const prezzoScontato = (costoBiglietto * sconto) / 100; // nuber
-    console.log("Prezzo scontato in euro" , prezzoScontato);
+        // Applicare lo sconto in base alla fascia d'eta
+        // creo variabile prezzoscontato
 
-    const prezzoFinale = costoBiglietto - prezzoScontato;
-    console.log("Prezzo Finale" , prezzoFinale);
+        const prezzoScontato = (costoBiglietto * sconto) / 100; // nuber
+        console.log("Prezzo scontato in euro", prezzoScontato);
 
+        const prezzoFinale = costoBiglietto - prezzoScontato;
+        console.log("Prezzo Finale", prezzoFinale);
+
+        document.getElementById("prezzoFinale").innerHTML = prezzoFinale;
+
+    
 })
+
+// creo il reset attraverso il tasto annulla per azzerare tutti i valori
+
+const resetta = document.getElementById("resetta");
+
+resetta.addEventListener ("click", function() {
+    document.getElementById("nomeUtente").value = "";
+    document.getElementById("prezzoAlKm").value = "";
+
+} )
+
